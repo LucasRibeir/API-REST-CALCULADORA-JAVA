f393c84e5a1df8000f8a6d8b7bde046031016bc0
Lucas Ribeiro <lucas.gordo@cassems.onmicrosoft.com>
2024-11-29 14:47:59 -0300

    Andamento da calculadora

---
+++ src/main/java/br/com/lucas/api_rest_spring_react/Gretting.java
@@ -0,0 +1,17 @@
+package br.com.lucas.api_rest_spring_react;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Gretting {
+
+    private final long id;
+    private final String content;
+
+    public Gretting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+}
---
+++ src/main/java/br/com/lucas/api_rest_spring_react/Startup.java
@@ -0,0 +1,13 @@
+package br.com.lucas.api_rest_spring_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
